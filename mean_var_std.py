e8aeabf6b97a87ec370ca8feb01eced4f680319c
EmmanuellaAE <97716020+EmmanuellaAE@users.noreply.github.com>
2022-10-13 21:03:40 -0500

    Update mean_var_std.py

--- mean_var_std.py
+++ mean_var_std.py
@@ -4,11 +4,11 @@ import numpy as np
 
 # Creating a function that returns aggregates of nine numbers.
 
-def calculate(nine_int):
-    if len(nine_int) != 9:
+def calculate(list):
+    if len(list) != 9:
         raise ValueError("List must contain nine numbers")
     else:
-        matrix = np.reshape(nine_int, (3, 3))
+        matrix = np.reshape(list, (3, 3))
 
     aggregates = {'mean:': [np.mean(matrix, axis =0, dtype='float16').tolist(),
                  np.mean(matrix, axis= 1, dtype= 'float16').tolist(), np.mean(matrix, dtype='float16')],
@@ -21,7 +21,4 @@ def calculate(nine_int):
     'sum_mat:' : [np.sum(matrix, axis= 0).tolist(), np.sum(matrix, axis= 1).tolist(), np.sum(matrix)]
     }
     
-    for key,value in aggregates.items():
-        print(key,value,'\n')
-
-calculate()
+    return aggregates
